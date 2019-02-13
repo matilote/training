2112aa6c4df190a9b7fda21d04a3089473086978
Grzegorz Wozniak <gwozniak@wecodefirst.com>
2019-02-13 19:20:48 +0000

    Zadanie

--- training-react/src/App/App.js
+++ training-react/src/App/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react'
 import './App.css'
-import { Input } from 'semantic-ui-react'
+import { Grid } from 'semantic-ui-react'
 
 class App extends Component {
+  
   render() {
+    const d = new Date()
     return (
       <div className="App">
-        <h1>Prosty komponent</h1>
-        <Input value="text" />
+        <Grid textAlign='center' columns={3}>
+          <Grid.Row>
+            <Grid.Column>
+              <section>
+                O mnie
+              </section>
+            </Grid.Column>
+            <Grid.Column>
+              <section>
+                Zainteresowania
+              </section>
+            </Grid.Column>
+            <Grid.Column>
+              <section>
+                Kontakt
+              </section>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </div>
     );
   }
---
+++ training-react/src/content.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    tc001: [
+      { name: 'Coś o mnie', text: 'To jednak jest dużo ważniejsze niż poprzedni tekst', created: '2019-02-09T18:13:47.627Z' },
+      { name: 'Coś innego o mnie', text: 'To jednak jest dużo ważniejsze niż poprzedni tekst', created: '2019-02-07T11:13:47.627Z' },
+      { name: 'v2 o mnie', text: 'To jest piękniejszy tekst, bardziej mi się podoba', created: '2019-02-03T10:33:47.627Z' },
+      { name: 'v1 o mnie', text: 'To jest pierwszy tekst jaki napisałem', created: '2019-02-02T14:13:47.627Z' }
+    ]
+  },
+  {
+    tc002: [
+      { name: 'To jest ciągle TODO', text: 'To wkrótce powstanie, obiecuję', created: '2019-02-06T18:13:47.627Z' },
+      { name: 'v1 o zainteresowaniach', text: 'To tu napiszę czym się interesuję', created: '2019-02-04T14:13:47.627Z' }
+    ]
+  },
+  {
+    tc003: [
+      { name: 'v2 kontakt', text: 'Napisz do mnie na e-mail: imie@nazwisko.pl', created: '2019-01-11T12:13:47.627Z' },
+      { name: 'v1 kontaktowe', text: 'Tu będą dane kontaktowe', created: '2019-01-02T11:13:47.627Z' }
+    ]
+  }
+]
