64009fb66258af127f5b1f71b173d13abf9c9c22
Kamila <kamila.m.lewicka@gmail.com>
2019-02-13 21:05:42 +0000

    Stara petla

--- training-react/src/App/App.js
+++ training-react/src/App/App.js
@@ -5,8 +5,29 @@ import { contentArray, contentObj } from '../content.js'
 
 class App extends Component {
   
+  // 1. Na 1. stopniu zagniezdzenia wystepuja na pewno obiekty, które zawierają klucz: tc001, tc002, tc003
+  // 2. Każdy z rekordów powyżej posiada co najmniej jeden rekord wewnątrz swojej tablicy
+
+  // contentArray[0]
+  // tc001
+  
+  // textOmnie += contentArray[i];
+
+  // {contentObj.tc003.i1.text}
+  
   render() {
-    const d = new Date()
+    
+    const id = 'tc001'
+    var rekordOmnie = ''
+    for (var klucz = 0; klucz < contentArray.length; klucz++) {
+      var rekord = contentArray[klucz]
+      if (rekord.hasOwnProperty(id)) {
+        rekordOmnie = rekord[id]
+      }
+    }
+    
+    console.log(rekordOmnie)
+
     return (
       <div className="App">
         <Grid textAlign='center' columns={3}>
@@ -14,19 +35,19 @@ class App extends Component {
             <Grid.Column>
               <section>
                 <h1>O mnie</h1>
-                <p>{contentObj.tc001.i1.text}</p>
+                <p></p>
               </section>
             </Grid.Column>
             <Grid.Column>
               <section>
                 <h1>Hobby</h1>
-                  <p>{contentObj.tc002.i1.text}</p>
+                  <p>{contentArray[1].text}</p>
               </section>
             </Grid.Column>
             <Grid.Column>
               <section>
                 <h1>Kontakt</h1>
-                <p>{contentObj.tc003.i1.text}</p>
+                <p>{contentArray[2]}</p>
               </section>
             </Grid.Column>
           </Grid.Row>
