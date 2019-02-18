754b263173c9de0e9c3855fc5380458f52d2f69b
Grzegorz Wozniak <gwozniak@wecodefirst.com>
2019-02-18 19:37:09 +0000

    Modyfikacja obiektu

--- training-react/src/content.js
+++ training-react/src/content.js
@@ -1,10 +1,10 @@
 export default [
-  { id: 1, name: 'Coś o mnie', text: 'To jednak jest dużo ważniejsze niż poprzedni tekst', created: '2019-02-09T18:13:47.627Z' },
-  { id: 2, name: 'Coś innego o mnie', text: 'To jednak jest dużo ważniejsze niż poprzedni tekst', created: '2019-02-07T11:13:47.627Z' },
-  { id: 3, name: 'v2 o mnie', text: 'To jest piękniejszy tekst, bardziej mi się podoba', created: '2019-02-03T10:33:47.627Z' },
-  { id: 4, name: 'v1 o mnie', text: 'To jest pierwszy tekst jaki napisałem', created: '2019-02-02T14:13:47.627Z' },
-  { id: 5, name: 'To jest ciągle TODO', text: 'To wkrótce powstanie, obiecuję', created: '2019-02-06T18:13:47.627Z' },
-  { id: 6, name: 'v1 o zainteresowaniach', text: 'To tu napiszę czym się interesuję', created: '2019-02-04T14:13:47.627Z' },
-  { id: 7, name: 'v2 kontakt', text: 'Napisz do mnie na e-mail: imie@nazwisko.pl', created: '2019-01-11T12:13:47.627Z' },
-  { id: 8, name: 'v1 kontaktowe', text: 'Tu będą dane kontaktowe', created: '2019-01-02T11:13:47.627Z' }
+  { id: 1, container: 'o-mnie', name: 'Coś o mnie', text: 'To jednak jest dużo ważniejsze niż poprzedni tekst', created: '2019-02-09T18:13:47.627Z' },
+  { id: 2, container: 'o-mnie', name: 'Coś innego o mnie', text: 'To jednak jest dużo ważniejsze niż poprzedni tekst', created: '2019-02-07T11:13:47.627Z' },
+  { id: 3, container: 'o-mnie', name: 'v2 o mnie', text: 'To jest piękniejszy tekst, bardziej mi się podoba', created: '2019-02-03T10:33:47.627Z' },
+  { id: 4, container: 'o-mnie', name: 'v1 o mnie', text: 'To jest pierwszy tekst jaki napisałem', created: '2019-02-02T14:13:47.627Z' },
+  { id: 5, container: 'hobby', name: 'To jest ciągle TODO', text: 'To wkrótce powstanie, obiecuję', created: '2019-02-06T18:13:47.627Z' },
+  { id: 6, container: 'hobby', name: 'v1 o zainteresowaniach', text: 'To tu napiszę czym się interesuję', created: '2019-02-04T14:13:47.627Z' },
+  { id: 7, container: 'kontakt', name: 'v2 kontakt', text: 'Napisz do mnie na e-mail: imie@nazwisko.pl', created: '2019-01-11T12:13:47.627Z' },
+  { id: 8, container: 'kontakt', name: 'v1 kontaktowe', text: 'Tu będą dane kontaktowe', created: '2019-01-02T11:13:47.627Z' }
 ]
