bd0c26db8d6897a9080e722f94e27a9e53fbfe80
Mateusz J <mateusz.jedrzejewski@yahoo.pl>
2019-04-08 22:23:07 +0200

    task

--- training-nesting/app.js
+++ training-nesting/app.js
@@ -1,13 +1,52 @@
 var schema = require('./simpleData')
 
+/*
+  _id: {
+    type: "number",
+    validation: undefined,
+    test: "xxx"
+  },
+*/
+
+// 1. Jak iterowac przez obiekt JS
+// 2. Jakie znamy petle w JS?
+//    for, while, do while, for in, for of, forEach, map
+
 var start = (obj) => {
-  const data = Object.assign({}, obj)
-  const id = Object.assign({}, data._id)
-  delete id.validation
-  delete id.test
+  const data = cloneObject(obj)
+  const id = cloneObject(data._id)
+
+  // delete id.validation
+  // delete id.test
+
+  // ---------------------------------
+  const obj = {
+    a: 1,
+    b: 2, 
+    c: 3,
+    d: 4
+  }
+
+  delete id.a
+  delete id.b
+  delete id.c
+  delete id.d
+
+  // ...
+
+  // Pętla
+
+  // Wynik: {}
+  // ---------------------------------
+
+
   return { _id: id }
 }
 
 module.exports = {
   start
+}
+
+const cloneObject = (source) => {
+  return Object.assign({}, source)
 }
